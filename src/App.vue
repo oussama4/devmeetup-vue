a35c61572c956662c3ab92ba3d445da6927aa4bc
oussama4 <oussamazouaki4@gmail.com>
2017-12-18 23:00:36 +0000

    first steps with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
   <v-app>
+    <v-navigation-drawer fixed temporary v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content> View Meetups </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark color="deep-orange darken-3">
+      <v-toolbar-side-icon
+        @click.native.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up">
+      </v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left dark>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main></main>
   </v-app>
 </template>
 
@@ -7,7 +32,7 @@
   export default {
     data () {
       return {
-
+        sideNav: false
       }
     }
   }
