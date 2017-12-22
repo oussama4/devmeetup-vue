a7b7b6943234f0d657b8f28f34e662f5d0149bf7
oussama4 <oussamazouaki4@gmail.com>
2017-12-22 23:32:31 +0000

    Adding a Date Filter

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import 'vuetify/dist/vuetify.css'
 import store from './store'
 import App from './App'
 import router from './router'
+import DateFilter from './filters/date'
 
 Vue.use(Vuetify)
+
+Vue.filter('date', DateFilter)
 
 Vue.config.productionTip = false
 
