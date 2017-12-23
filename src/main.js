9ae1b0e9fa8b689512db39b8abcf772adddb9364
oussama4 <oussamazouaki4@gmail.com>
2017-12-23 19:55:13 +0000

    A Signup View & Form

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
+import * as firebase from 'firebase'
 
 import store from './store'
 import App from './App'
@@ -19,5 +20,14 @@ new Vue({
   el: '#app',
   store,
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD1GRFRrO9Uu-cbmbiQvWXVlGJPvWaVlgk',
+      authDomain: 'devmeetup-vue-53284.firebaseapp.com',
+      databaseURL: 'https://devmeetup-vue-53284.firebaseio.com',
+      projectId: 'devmeetup-vue-53284',
+      storageBucket: 'devmeetup-vue-53284.appspot.com'
+    })
+  }
 })
